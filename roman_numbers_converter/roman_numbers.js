8f62f4468f4e414d1e8e76c7f23dc2b6fe1925a5
relikt <reliktus@gmail.com>
2017-06-05 12:42:40 +0200

    Refactoring

--- roman_numbers_converter/roman_numbers.js
+++ roman_numbers_converter/roman_numbers.js
@@ -11,115 +11,67 @@ function convertToRoman(num) {
     });
 
     function numberOne(data) {
-        for ( var i=0; i < data; i++) {
+        for ( i=0; i < data; i++) {
             roman += "M";
         }
     }
 
-    function numberTwo(data) {
+    function numberTransform(data,low,mid,high) {
         if ( data <= 3) {
-            for ( var i=0; i < data; i++) {
-                roman += "C";
+            for ( i=0; i < data; i++) {
+                roman += low;
             }
         } else if ( data === 4 ) {
-            roman += "CD";
+            roman += low + mid;
         } else if ( data < 9) {
-            roman += "D";
+            roman += mid;
             for ( i=0; i < (data-5) && data > 5; i++) {
-                roman += "C";
+                roman += low;
             }
         } else {
-            roman += "CM";
+            roman += low + high;
         }
     }
-
-    function numberThree(data) {
-        if ( data <= 3) {
-            for ( var i=0; i < data; i++) {
-                roman += "X";
-            }
-        } else if ( data === 4 ) {
-            roman += "XL";
-        } else if ( data < 9) {
-            roman += "L";
-            for ( i=0; i < (data-5) && data > 5; i++) {
-                roman += "X";
-            }
-        } else {
-            roman += "XC";
-        }
-    }
-
-    function numberFour(data) {
-        if ( data <= 3) {
-            for ( var i=0; i < data; i++) {
-                roman += "I";
-            }
-        } else if ( data === 4 ) {
-            roman += "IV";
-        } else if ( data < 9) {
-            roman += "V";
-            for ( i=0; i < (data-5) && data > 5; i++) {
-                roman += "I";
-            }
-        } else {
-            roman += "IX";
-        }
-    }
-
-    /* function transformNum(numb, romA, romB, romC) {
-        if ( numb <= 3) {
-            for ( var i=0; i < numb; i++) {
-                roman += "I";
-            }
-        } else if ( numb === 4 ) {
-            roman += "IV";
-        } else if ( numb < 9) {
-            roman += "V";
-            for ( i=0; i < (numb-5) && numb > 5; i++) {
-                roman += "I";
-            }
-        } else {
-            roman += "IX";
-        }
-    } */
 
     function checkArr() {
         if ( numArr.length >= 4 )
         {
             numberOne(numArr[0]);
-            numberTwo(numArr[1]);
-            numberThree(numArr[2]);
-            numberFour(numArr[3]);
+            numberTransform(numArr[1],"C","D","M");
+            numberTransform(numArr[2],"X","L","C");
+            numberTransform(numArr[3],"I","V","X");
         }
         else if ( numArr.length === 3 )
         {
-            numberTwo(numArr[0]);
-            numberThree(numArr[1]);
-            numberFour(numArr[2]);
+            numberTransform(numArr[0],"C","D","M");
+            numberTransform(numArr[1],"X","L","C");
+            numberTransform(numArr[2],"I","V","X");
         }
         else if ( numArr.length === 2 )
         {
-            numberThree(numArr[0]);
-            numberFour(numArr[1]);
+            numberTransform(numArr[0],"X","L","C");
+            numberTransform(numArr[1],"I","V","X");
         }
         else
         {
-            numberFour(numArr[0]);
+            numberTransform(numArr[0],"I","V","X");
         }
     }
 
-    checkArr();
-    console.log(roman);
 
-     $('#romanNumber').html(roman);
+    checkArr();
+    $('#romanNumber').html(roman);
+
+
+
 }
 
 function buttonConvert() {
     $('#convert').click(function () {
+        $('#romanNumber').hide(10);
         var numVal = document.getElementById("toConvert").value;
-        // console.log(numVal);
         convertToRoman(numVal);
+        $('#romanNumber').show(500);
     })
 }
 
